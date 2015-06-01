b6fc4bb1feb77df3e8537f4f951c47a36c8fb470
Sarmen <Khodjasarian@csupomona.edu>
2015-05-31 23:35:49 -0700

    delete workflowinstance added

--- workflowInstance.js
+++ workflowInstance.js
@@ -92,3 +92,27 @@ function updateSubmittedBy(workflowInstanceId, sumbittedBy)
     }
   });
 }
+function deleteWorkflowInstance(workflowInstanceId)
+{
+  Parse.initialize("ej29LXB9zHARKwcF5gHhkQ4SnJS7mGwWZ01qrZAa", "jTpvM9KVA9G9XteMyDD4nDcL6xNPVhg44zliTSrw");
+
+  var WorkflowInstance = Parse.Object.extend("WorkflowInstance");
+  var workflowInstance = new WorkflowInstance();
+  var query = new Parse.Query(WorkflowInstance);
+  query.equalTo("WorkflowInstanceId", workflowInstanceId);
+    query.first({
+      success: function(results) {
+        results.destroy({
+          success: function(myObject) {
+   
+          error: function(myObject, error) {
+  }
+})
+          }
+        });  
+    },
+    error:function(results) {
+      alert("woop");
+    }
+  });
+}
